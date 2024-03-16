ca9bca101c9be4cc47d90fad36a8ee9d772c2b3c
giggi1226 <gpacheco003@gmail.com>
2024-03-16 17:12:24 -0400

    customizing navigation

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,19 +2,22 @@ import Link from 'next/link'
 
 export const NavBar = () => {
   return (
-    <ul className="flex bg-blue text-offWhite">
-      <li>
-        <Link href="/blog">Blog</Link>
-      </li>
-      <hr className='border-r-2 border-offWhite h-auto m-1'/>
-      <li>
-        <Link href="https://github.com/giggi1226" target="_blank">GitHub</Link>
-      </li>
-      <hr className='border-r-2 border-offWhite h-auto m-1'/>
-      <li>
-        <Link href="https://linkedin.com/in/gipacheco" target="_blank">Linkedin</Link>
-      </li>
-    </ul>
+    <div className="flex bg-blue text-offWhite h-10 justify-between px-3 items-center">
+      <h1>Giselle Pacheco</h1>
+      <ul className="flex">
+        <li>
+          <Link href="https://tech.gisellep.com">Blog</Link>
+        </li>
+        <hr className='border-r-2 border-offWhite h-auto m-1'/>
+        <li>
+          <Link href="https://github.com/giggi1226" target="_blank">GitHub</Link>
+        </li>
+        <hr className='border-r-2 border-offWhite h-auto m-1'/>
+        <li>
+          <Link href="https://linkedin.com/in/gipacheco" target="_blank">Linkedin</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
